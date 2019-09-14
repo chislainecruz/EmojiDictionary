47ec433fd4db59f754a4f72c3505711fd3c9ea51
Chislaine Cruz <chislaine.cruz@gmail.com>
2019-09-14 10:48:27 -0400

    Initial Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by Chislaine Cruz on 9/14/19.
+//  Copyright © 2019 Chislaine Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
