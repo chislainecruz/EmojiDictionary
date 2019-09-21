9a8f7a11d0c76d2c19eb2d66ac34b2f0eab41cc0
Chislaine Cruz <chislaine.cruz@gmail.com>
2019-09-21 10:36:08 -0400

    All three encoding/decoding methods implemented.

--- EmojiDictionary/Emoji.swift
+++ EmojiDictionary/Emoji.swift
@@ -21,21 +21,34 @@ class Emoji : Codable {
         self.usage = usage
     }
     
-    static func saveToFile (emojis: [Emoji]) {
+    static func saveToFile(emojis: [Emoji]) {
        //use property list encoder to encode Emoji object. Save and write
+        let propertyListEncoder = PropertyListEncoder()
+        let encodedEmoji = try? propertyListEncoder.encode(emojis)
+        try? encodedEmoji?.write(to: Emoji.ArchiveURL, options: .noFileProtection)
     }
         
     static func loadFromFile() -> [Emoji] {
         //use property list decoder Emoji object. Read and and decode
-        
-        return []
+        let propertyListDecoder = PropertyListDecoder()
+        if let retreivedEmojiData = try? Data(contentsOf: Emoji.ArchiveURL),
+            let decodedEmoji = try? propertyListDecoder.decode(Array<Emoji>.self, from: retreivedEmojiData){
+                return decodedEmoji
+        }
+        return loadSampleEmojis()
     }
     
-    static func archiveURL() -> URL {
+    static var ArchiveURL: URL {
         let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let archiveURL = documentsDirectory.appendingPathComponent("emojis").appendingPathExtension("plist")
         
         return archiveURL
+    }
+    
+    static func loadSampleEmojis() -> [Emoji] {
+        let sample = EmojiTableTableViewController()
+        return sample.emojis
+        
     }
 
    
